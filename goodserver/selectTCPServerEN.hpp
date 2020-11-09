03688481d0fd236f0b001c02324aba8c6fcab86d
SoulBasic <690204874@qq.com>
2020-11-09 18:17:14 +0800

    add en

--- goodserver/selectTCPServerEN.hpp
+++ goodserver/selectTCPServerEN.hpp
@@ -69,7 +69,7 @@ public:
 	TCPServer(const TCPServer& other) = delete;
 	const TCPServer& operator=(const TCPServer& other) = delete;
 public:
-	//初始化win环境
+
 	explicit inline TCPServer()
 	{
 		ssock = INVALID_SOCKET;
@@ -82,10 +82,10 @@ public:
 		terminal();
 	}
 
-	//判断服务器是否正常运行中
+
 	inline bool active() { return ssock != INVALID_SOCKET; }
 
-	//初始化socket
+
 	int initSocket()
 	{
 #ifdef _WIN32
@@ -93,22 +93,22 @@ public:
 		WSADATA data;
 		if (SOCKET_ERROR == WSAStartup(version, &data))
 		{
-			std::cout << u8"初始化Winsock环境失败" << std::endl;
+			std::cout << "Failed to initialize Winsock environment" << std::endl;
 		}
 		else
 		{
-			std::cout << u8"已成功初始化Winsock环境!" << std::endl;
+			std::cout << "Winsock environment initialized successfully!" << std::endl;
 		}
 #endif 
 		ssock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 		if (INVALID_SOCKET == ssock)
 		{
-			std::cout << u8"初始化服务器失败" << std::endl;
+			std::cout << "Failed to initialize the server" << std::endl;
 			return CMD_ERROR;
 		}
 		else
 		{
-			std::cout << u8"服务器初始化成功!" << std::endl;
+			std::cout << "Server initialization successful!" << std::endl;
 		}
 		return CMD_SUCCESS;
 	}
@@ -118,7 +118,7 @@ public:
 	{
 		if (INVALID_SOCKET == ssock)
 		{
-			std::cout << u8"套接字未初始化或无效" << std::endl;
+			std::cout << "Socket is not initialized or invalid" << std::endl;
 			return CMD_ERROR;
 		}
 		ssin.sin_family = AF_INET;
@@ -131,22 +131,22 @@ public:
 		int res = bind(ssock, (sockaddr*)&ssin, sizeof(ssin));
 		if (SOCKET_ERROR == res)
 		{
-			std::cout << u8"绑定端口失败" << std::endl;
+			std::cout << "Failed to bind port" << std::endl;
 			return CMD_ERROR;
 		}
 		else
 		{
-			std::cout << u8"端口绑定成功!" << std::endl;
+			std::cout << "Port bound successfully!" << std::endl;
 		}
 
 		if (SOCKET_ERROR == listen(ssock, 5))
 		{
-			std::cout << u8"侦听端口失败" << std::endl;
+			std::cout << "Listening port failed" << std::endl;
 			return -1;
 		}
 		else
 		{
-			std::cout << u8"侦听端口成功!" << std::endl;
+			std::cout << "Listening port succeeded!" << std::endl;
 		}
 
 		return CMD_SUCCESS;
@@ -174,11 +174,11 @@ public:
 		//std::cout << "select result = " << res << " num = " << num++ << std::endl;
 		if (res < 0)
 		{
-			std::cout << u8"select模型未知错误，任务结束" << std::endl;
+			std::cout << "Select model unknown error, task ended" << std::endl;
 			terminal();
 			return false;
 		}
-		if (FD_ISSET(ssock, &fdRead))//新客户加入
+		if (FD_ISSET(ssock, &fdRead))
 		{
 			acceptClient();
 			FD_CLR(ssock, &fdRead);
@@ -196,30 +196,30 @@ public:
 		return true;
 	}
 
-	//给客户端发消息
+
 	template<typename PackType>
 	int sendMessage(SOCKET csock, PackType* msg)
 	{
 		if (INVALID_SOCKET == ssock)
 		{
-			std::cout << u8"服务器套接字未初始化或无效" << std::endl;
+			std::cout << "The server socket is not initialized or invalid" << std::endl;
 			return CMD_ERROR;
 		}
 		int res = send(csock, (const char*)msg, sizeof(PackType), 0);
 		if (SOCKET_ERROR == res)
 		{
-			std::cout << u8"发送数据包失败" << std::endl;
+			std::cout << "Sending packet failed" << std::endl;
 			return CMD_ERROR;
 		}
 		else
 		{
 
-			//std::cout << "发送数据包to(" << csock << " ) " << msg->CMD << " " << msg->LENGTH << std::endl;
+			//std::cout << "Send packets to(" << csock << " ) " << msg->CMD << " " << msg->LENGTH << std::endl;
 		}
 		return CMD_SUCCESS;
 	}
 
-	//关闭socket
+
 	void terminal()
 	{
 		if (INVALID_SOCKET == ssock)return;
@@ -250,7 +250,7 @@ public:
 		case CMD_PRIVATEMESSAGE:
 		{
 			PrivateMessagePack* pack = static_cast<PrivateMessagePack*>(pk);
-			std::cout << u8"转发私信 " << std::endl;
+			std::cout << "Forward private message " << std::endl;
 			std::string sourceName = "user";
 			SOCKET target = 0;
 			auto it = clients.begin();
@@ -271,7 +271,7 @@ public:
 			else
 			{
 				MessagePack pack1;
-				strcpy(pack1.message, u8"私信发送失败，目标用户不存在或已离线");
+				strcpy(pack1.message, "Failed to send private message. The target user does not exist or is offline");
 				sendMessage(c->getSock(), &pack1);
 			}
 			break;
@@ -279,15 +279,15 @@ public:
 		case CMD_MESSAGE:
 		{
 			MessagePack* pack = static_cast<MessagePack*>(pk);
-			std::cout << u8"从客户端(" << c->getSock() << u8")收到的消息 :CMD=" << pack->CMD << u8" LENGTH=" << pack->LENGTH << u8" DATA=" << pack->message << std::endl;
-			strcpy(pack->message, u8"消息已成功被服务器接收!");
+			std::cout << "From client(" << c->getSock() << ")Message received :CMD=" << pack->CMD << " LENGTH=" << pack->LENGTH << " DATA=" << pack->message << std::endl;
+			strcpy(pack->message, "Message received by server successfully!");
 			sendMessage(c->getSock(), pack);
 			break;
 		}
 		case CMD_BROADCAST:
 		{
 			BroadcastPack* pack = static_cast<BroadcastPack*>(pk);
-			std::cout << u8"广播消息" << std::endl;
+			std::cout << "Broadcast news" << std::endl;
 			for (auto c1 : clients)
 			{
 				sendMessage(c1->getSock(), pack);
@@ -313,27 +313,27 @@ public:
 			if (it != clients.end())
 			{
 
-				std::cout << u8"用户" << oldName << u8"(" << c->getSock() << u8")改名为" << userName << std::endl;
-				userName = u8"已成功更改名称，现在的昵称为 " + userName;
+				std::cout << "user " << oldName << "(" << c->getSock() << ")Renamed as" << userName << std::endl;
+				userName = "The name has been successfully changed, now nickname is " + userName;
 				MessagePack pack1(userName.c_str());
 				sendMessage(c->getSock(), &pack1);
 			}
 			else
 			{
-				MessagePack pack1(u8"重命名失败");
+				MessagePack pack1("Rename failed");
 				sendMessage(c->getSock(), &pack1);
 			}
 			break;
 		}
 		case CMD_TEST:
 		{
-			//TestPack pack("djawodawdadjaiwodjoiawdjawjdawodijawidawjd的简欧外倒角温度计我阿的旧爱我ID熬完我鸡毛我万达茂温度计奥温度计啊 就奥的味道就");
+			//TestPack pack("djawodawdadjaiwodjoiawdjawjdawodijawidawjddwadwadada");
 			//sendMessage(c->getSock(), &pack);
 			break;
 		}
 		default:
 		{
-			std::cout << u8"无法解析的消息:CMD=" << pk->CMD << u8" length=" << pk->LENGTH << std::endl;
+			std::cout << "Unresolved message:CMD=" << pk->CMD << " length=" << pk->LENGTH << std::endl;
 			break;
 		}
 		}
@@ -341,7 +341,7 @@ public:
 
 private:
 
-	//接收连接的客户端
+
 	CLIENT* acceptClient()
 	{
 		SOCKET csock = INVALID_SOCKET;
@@ -356,11 +356,11 @@ private:
 
 		if (INVALID_SOCKET == csock)
 		{
-			std::cout << u8"无效的客户端套接字" << std::endl;
+			std::cout << "Invalid client socket" << std::endl;
 		}
 		else
 		{
-			std::cout << u8"新客户端连接:" << csock << " IP:" << inet_ntoa(csin.sin_addr) << std::endl;
+			std::cout << "New client connection:" << csock << " IP:" << inet_ntoa(csin.sin_addr) << std::endl;
 		}
 		std::string username = "user" + std::to_string(csock);
 		CLIENT* c = new CLIENT(csock, csin, csock, username);
@@ -369,7 +369,7 @@ private:
 
 	}
 
-	//接收并处理数据包
+
 	int recvPack(CLIENT* c)
 	{
 
@@ -378,7 +378,7 @@ private:
 		int len = recv(csock, recvBuf, RECV_BUF_SIZE, NULL);
 		if (len <= 0)
 		{
-			std::cout << u8"客户" << c->getUserName() << u8"(csock=" << csock << u8")已断开连接" << std::endl;
+			std::cout << "client " << c->getUserName() << "(csock=" << csock << ")Disconnected" << std::endl;
 			for (auto it = clients.begin(); it < clients.end(); it++)
 			{
 				if ((*it)->getSock() == c->getSock())
